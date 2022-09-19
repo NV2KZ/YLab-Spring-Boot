76e22915eaacaf85359f1609278821ad7c73c255
Nikita Kazakov <nvkazakov98@yandex.ru>
2022-09-19 13:59:40 +0300

    add id fields to requests

--- src/main/java/com/edu/ulab/app/web/request/BookRequest.java
+++ src/main/java/com/edu/ulab/app/web/request/BookRequest.java
@@ -1,9 +1,13 @@
 package com.edu.ulab.app.web.request;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import lombok.Data;
 
 @Data
+@JsonInclude(Include.NON_NULL)
 public class BookRequest {
+    private Long id;
     private String title;
     private String author;
     private long pageCount;
