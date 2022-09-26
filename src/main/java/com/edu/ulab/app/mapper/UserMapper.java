9e6a5a134add4d22f37c8c1afe0a7c342c91a85e
Nikita Kazakov <nvkazakov98@yandex.ru>
2022-09-26 16:24:21 +0300

    add mapping methods and javadocs

--- src/main/java/com/edu/ulab/app/mapper/UserMapper.java
+++ src/main/java/com/edu/ulab/app/mapper/UserMapper.java
@@ -1,15 +1,40 @@
 package com.edu.ulab.app.mapper;
 
 import com.edu.ulab.app.dto.UserDto;
-import com.edu.ulab.app.entity.User;
+import com.edu.ulab.app.entity.UserEntity;
 import com.edu.ulab.app.web.request.UserRequest;
 import org.mapstruct.Mapper;
 
+/**
+ * Interface for mapping operations for a User type.
+ */
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    /**
+     * Map userRequest to userDto.
+     * @param userRequest – must not be null.
+     * @return UserDto object.
+     */
     UserDto userRequestToUserDto(UserRequest userRequest);
 
+    /**
+     * Map userDto to userRequest.
+     * @param userDto – must not be null.
+     * @return UserRequest object.
+     */
     UserRequest userDtoToUserRequest(UserDto userDto);
 
-    UserDto userEntityToUserDto(User user);
+    /**
+     * Map userEntity to userDto.
+     * @param user – object of UserEntity. Must not be null.
+     * @return UserDto object.
+     */
+    UserDto userEntityToUserDto(UserEntity user);
+
+    /**
+     * Map userDto to userEntity.
+     * @param userDto – must not be null.
+     * @return UserEntity object.
+     */
+    UserEntity userDtoToUserEntity(UserDto userDto);
 }
