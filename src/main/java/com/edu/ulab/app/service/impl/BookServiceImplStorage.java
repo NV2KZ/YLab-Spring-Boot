0a5e6afbc722ea662e57e58cce22e632759eee5c
Nikita Kazakov <nvkazakov98@yandex.ru>
2022-09-26 16:29:28 +0300

    add implementation of services

--- src/main/java/com/edu/ulab/app/service/impl/BookServiceImplStorage.java
+++ src/main/java/com/edu/ulab/app/service/impl/BookServiceImplStorage.java
@@ -4,38 +4,35 @@ import com.edu.ulab.app.dto.BookDto;
 import com.edu.ulab.app.entity.Book;
 import com.edu.ulab.app.exception.NotFoundException;
 import com.edu.ulab.app.mapper.BookMapper;
-import com.edu.ulab.app.repository.BookRepository;
 import com.edu.ulab.app.service.BookService;
+import com.edu.ulab.app.storage.BookStorage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Slf4j
 @Service
-@Primary
 @RequiredArgsConstructor
-public class BookServiceImpl implements BookService {
-
-    private final BookRepository bookRepository;
+public class BookServiceImplStorage implements BookService {
+    private final BookStorage bookStorage;
     private final BookMapper bookMapper;
 
     @Override
     public BookDto createBook(BookDto bookDto) {
         Book newBook = bookMapper.bookDtoToBookEntity(bookDto);
         log.info("Mapped book: {}", newBook);
-        Book savedBook = bookRepository.save(newBook);
+        Book savedBook = bookStorage.save(newBook);
         log.info("Saved book: {}", savedBook);
         return bookMapper.bookEntityToBookDto(savedBook);
     }
 
     @Override
     public BookDto updateBook(BookDto bookDto) {
-        Book updatedBook = bookRepository.findById(bookDto.getId())
+        Book updatedBook = bookStorage.findById(bookDto.getId())
                 .map(book -> bookMapper.bookDtoToBookEntity(bookDto))
-                .map(bookRepository::save)
+                .map(bookStorage::save)
                 .orElseThrow(() -> new NotFoundException(String.format("Book with id = %s not found", bookDto.getId())));
         log.info("Updated book: {}", updatedBook);
         return bookMapper.bookEntityToBookDto(updatedBook);
@@ -43,25 +40,23 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public BookDto getBookById(Long id) {
-        Book book = bookRepository.findById(id)
+        Book book = bookStorage.findById(id)
                 .orElseThrow(() -> new NotFoundException(String.format("Book with id = %s not found", id)));
         log.info("Received book: {}", book);
         return bookMapper.bookEntityToBookDto(book);
     }
 
     public List<BookDto> getBooksByUserId(Long userId) {
-        log.info("Received books by userId = {}", userId);
-        return bookRepository.findAllBooksByUserId(userId)
+        return bookStorage.findAll()
                 .stream()
+                .filter(book -> book.getUserId().equals(userId))
                 .map(bookMapper::bookEntityToBookDto)
                 .toList();
     }
 
-
     @Override
     public void deleteBookById(Long id) {
-        bookRepository.deleteById(id);
-        log.info("Delete book by id = {}", id);
+        bookStorage.deleteById(id);
     }
-
 }
+
--- src/main/java/com/edu/ulab/app/service/impl/UserServiceImplStorage.java
+++ src/main/java/com/edu/ulab/app/service/impl/UserServiceImplStorage.java
@@ -4,36 +4,34 @@ import com.edu.ulab.app.dto.UserDto;
 import com.edu.ulab.app.entity.UserEntity;
 import com.edu.ulab.app.exception.NotFoundException;
 import com.edu.ulab.app.mapper.UserMapper;
-import com.edu.ulab.app.repository.UserRepository;
 import com.edu.ulab.app.service.UserService;
+import com.edu.ulab.app.storage.UserStorage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-@Primary
 @RequiredArgsConstructor
-public class UserServiceImpl implements UserService {
+public class UserServiceImplStorage implements UserService {
 
-    private final UserRepository userRepository;
+    private final UserStorage userStorage;
     private final UserMapper userMapper;
 
     @Override
     public UserDto createUser(UserDto userDto) {
         UserEntity userNew = userMapper.userDtoToUserEntity(userDto);
         log.info("Mapped user: {}", userNew);
-        UserEntity savedUser = userRepository.save(userNew);
+        UserEntity savedUser = userStorage.save(userNew);
         log.info("Saved user: {}", savedUser);
         return userMapper.userEntityToUserDto(savedUser);
     }
 
     @Override
     public UserDto updateUser(UserDto userDto) {
-        UserEntity updatedUser = userRepository.findById(userDto.getId())
+        UserEntity updatedUser = userStorage.findById(userDto.getId())
                 .map(user -> userMapper.userDtoToUserEntity(userDto))
-                .map(userRepository::save)
+                .map(userStorage::save)
                 .orElseThrow(() -> new NotFoundException(String.format("User with id = %s not found", userDto.getId())));
         log.info("Updated user: {}", updatedUser);
         return userMapper.userEntityToUserDto(updatedUser);
@@ -41,7 +39,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDto getUserById(Long id) {
-        UserEntity user = userRepository.findById(id)
+        UserEntity user = userStorage.findById(id)
                 .orElseThrow(() -> new NotFoundException(String.format("User with id = %s not found", id)));
         log.info("Received user: {}", user);
         return userMapper.userEntityToUserDto(user);
@@ -49,7 +47,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void deleteUserById(Long id) {
-        userRepository.deleteById(id);
-        log.info("Delete user by id = {}", id);
+        userStorage.deleteById(id);
+        log.info("Delete book by id = {}", id);
     }
 }
