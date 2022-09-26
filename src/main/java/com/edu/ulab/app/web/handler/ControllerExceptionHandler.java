07be024cdae172b111ff6a8ea16aeb24e6b347db
Nikita Kazakov <nvkazakov98@yandex.ru>
2022-09-26 16:31:49 +0300

    add handle methods for some exceptions

--- src/main/java/com/edu/ulab/app/web/handler/ControllerExceptionHandler.java
+++ src/main/java/com/edu/ulab/app/web/handler/ControllerExceptionHandler.java
@@ -6,6 +6,7 @@ import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -17,7 +18,30 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<BaseWebResponse> handleNotFoundExceptionException(@NonNull final NotFoundException exc) {
         log.error(exc.getMessage());
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                .body(new BaseWebResponse(createErrorMessage(exc)));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<BaseWebResponse> handleIllegalArgumentException(@NonNull final IllegalArgumentException exc) {
+        log.error(exc.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new BaseWebResponse(createErrorMessage(exc)));
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<BaseWebResponse> handleMissingServletRequestParameterException(
+            @NonNull final IllegalArgumentException exc
+    ) {
+        log.error(exc.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new BaseWebResponse(createErrorMessage(exc)));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<BaseWebResponse> handleException(@NonNull final Exception exc) {
+        log.error(exc.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new BaseWebResponse(createErrorMessage(exc)));
     }
 
