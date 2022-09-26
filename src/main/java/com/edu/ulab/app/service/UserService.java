935f0c7d20a1fa74508c5669066bd0fdb4c3107e
Nikita Kazakov <nvkazakov98@yandex.ru>
2022-09-26 16:28:06 +0300

    add javadocs

--- src/main/java/com/edu/ulab/app/service/UserService.java
+++ src/main/java/com/edu/ulab/app/service/UserService.java
@@ -2,12 +2,34 @@ package com.edu.ulab.app.service;
 
 import com.edu.ulab.app.dto.UserDto;
 
+/**
+ * Interface for CRUD operations for a User.
+ */
 public interface UserService {
+    /**
+     * Create new UserEntity in database. Generate the id and insert new UserEntity in database.
+     * @param userDto – must not be null.
+     * @return userDto object with generated id
+     */
     UserDto createUser(UserDto userDto);
 
+    /**
+     * Update UserEntity for given userDto in database. Update fields of an existing UserEntity in database.
+     * @param userDto – must not be null.
+     * @return userDto object with updated fields.
+     */
     UserDto updateUser(UserDto userDto);
 
+    /**
+     * Return userDto object for given id from database.
+     * @param id – must not be null.
+     * @return userDto for given id
+     */
     UserDto getUserById(Long id);
 
+    /**
+     * Delete UserEntity for given id from database.
+     * @param id – must not be null.
+     */
     void deleteUserById(Long id);
 }
