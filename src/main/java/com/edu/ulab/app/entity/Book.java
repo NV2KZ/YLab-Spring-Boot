465e6d180d34b994022e4de7e95db5ad13995811
Nikita Kazakov <nvkazakov98@yandex.ru>
2022-10-03 20:30:11 +0300

    add sequence id generation

--- src/main/java/com/edu/ulab/app/entity/Book.java
+++ src/main/java/com/edu/ulab/app/entity/Book.java
@@ -1,20 +1,31 @@
 package com.edu.ulab.app.entity;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
+@Table(name = "book", schema = "ulab_edu")
 public class Book {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence")
+    @SequenceGenerator(name = "sequence", sequenceName = "sequence", allocationSize = 100)
     private Long id;
+
+    @Column(nullable = false)
     private Long userId;
+
+    @Column(nullable = false)
     private String title;
+
+    @Column(nullable = false)
     private String author;
+
+    @Column(nullable = false)
     private long pageCount;
 }
--- src/main/java/com/edu/ulab/app/entity/UserEntity.java
+++ src/main/java/com/edu/ulab/app/entity/UserEntity.java
@@ -1,17 +1,30 @@
 package com.edu.ulab.app.entity;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
-@Table(name = "Person")
+@Table(name = "person", schema = "ulab_edu",
+        indexes = {@Index(name = "idx_ulab_edu_person_title",
+        columnList = "title")})
 public class UserEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence")
+    @SequenceGenerator(name = "sequence", sequenceName = "sequence", allocationSize = 100)
     private Long id;
+
+    @Column(nullable = false)
     private String fullName;
+
+    @Column(nullable = false)
     private String title;
+
+    @Column(nullable = false)
     private int age;
 }
